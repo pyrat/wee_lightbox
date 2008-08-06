c73d5630bb3ada128471af7b4ca5c8bf9d83db21
Alastair Brunton <info@simplyexcited.co.uk>
2008-08-06 23:17:01 +0200

    hopefully the dot will do it with the installer!

--- install.rb
+++ install.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 puts "Installing into your rails app.."
 plugin_name = 'wee_lightbox'
 
-src =  "#{RAILS_ROOT}/vendor/plugins/#{plugin_name}/files/"
+src =  "#{RAILS_ROOT}/vendor/plugins/#{plugin_name}/files/."
 dest = "#{RAILS_ROOT}/public"
 options = {:verbose => true}
 
