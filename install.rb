2940ba3d9fbdbc5f1079767a831e39dbea851823
Alastair Brunton <info@simplyexcited.co.uk>
2008-08-06 22:59:58 +0200

    addition of git ignore and install script inspired by looking at the fckeditor plugin install

--- install.rb
+++ install.rb
@@ -1,1 +1,12 @@
-# Install hook code here
+require 'fileutils'
+
+puts "Installing into your rails app.."
+plugin_name = 'wee_lightbox'
+
+src =  "#{RAILS_ROOT}/vendor/plugins/#{plugin_name}/files/"
+dest = "#{RAILS_ROOT}/public/"
+options = {:verbose => true}
+
+FileUtils.cp_r(src, dest, options)
+
+puts "All done!"
