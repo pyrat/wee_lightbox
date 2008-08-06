f74c0e91ed2b55ef02669629aff7eb82ede18244
Alastair Brunton <info@simplyexcited.co.uk>
2008-08-06 23:04:44 +0200

    bugix in install script

--- install.rb
+++ install.rb
@@ -3,8 +3,8 @@ require 'fileutils'
 puts "Installing into your rails app.."
 plugin_name = 'wee_lightbox'
 
-src =  "#{RAILS_ROOT}/vendor/plugins/#{plugin_name}/files/"
-dest = "#{RAILS_ROOT}/public/"
+src =  "#{RAILS_ROOT}/vendor/plugins/#{plugin_name}/files"
+dest = "#{RAILS_ROOT}/public"
 options = {:verbose => true}
 
 FileUtils.cp_r(src, dest, options)
